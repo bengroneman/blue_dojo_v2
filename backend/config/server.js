e39c2fa725af3bd13dab1b2454d8e876bd835fde
bengroneman <bluegroneman10@gmail.com>
2021-12-12 18:48:10 -0700

    rebuild

--- backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '7088ae2716db8ec64e0ba26345140a79'),
-    },
-  },
-});
